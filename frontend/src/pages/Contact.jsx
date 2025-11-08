3ba5d4255e7fd8f88105dedfd4319cc60be71aaa
Nayan <dreamerboynayan@gmail.com>
2025-11-08 08:50:29 +0600

    Contact page work completed

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import {assets} from '../assets/assets'
 
 const Contact = () => {
   return (
     <div>
+      <div className='text-center text-2xl pt-10 text-gray-500'>
+        <p >CONTACT <span className='text-gray-700 font-semibold'>US</span></p>
+      </div>
+      <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28 text-sm'>
+        <img className='w-full md:max-w-[360px]' src={assets.contact_image} alt="" />
+        <div className='flex flex-col justify-center items-start gap-6'>
+        <p className='font-semibold text-lg text-gray-600 '>Our OFFICE</p>
+        <p className='text-gray-500'>54709 Willms Station <br /> Suite 350, Washington, USA</p>
+        <p className='text-gray-500'>Tel: (415) 555‑0132 <br /> Email: pulsecare247@gmail.com</p>
+        <p className='font-semibold text-lg text-gray-600 '>Careers at PULSECARE</p>
+        <p className='text-gray-500'>Learn more about our teams and job openings.</p>
+        <p className='border border-pink-400 px-8 py-4 text-sm hover:bg-pink-400 hover:text-white transition-all duration-500 cursor-pointer'>Explore Jobs</p>
+      </div>
+      </div>
+     
       
     </div>
   )
