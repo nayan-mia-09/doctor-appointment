402ed4e33e776e1a9939491fc33bf96eabda53ef
Nayan <dreamerboynayan@gmail.com>
2025-11-24 19:01:05 +0600

    Backend work started

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,28 @@
-import mongoose from "mongoose";
-import "dotenv/config"
-import app from "./index.js";
-import { MONGO_URI, PORT } from "./config/config.js";
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./config/mongodb.js";
+import connectionCloudinary from "./config/cloudnary.js";
+import adminRouter from "./routes/adminRoutes.js";
 
+// App config
 
+const app = express();
+const port = process.env.PORT || 8000;
+connectDB();
+connectionCloudinary();
 
-mongoose.connect(MONGO_URI).then(()=>{
-    console.log(`Database Connected.`);
-    app.listen(PORT, ()=>{
-        console.log(`Server Started at PORT ${PORT}`)
-    })
-    
-}).catch((err)=>{
-console.log(`Database Connection Error:`,err)
-})
+// middlewares
+
+app.use(express.json());
+app.use(cors());
+
+// api endpoints
+
+app.use('/api/admin', adminRouter)
+
+app.get("/", (req, res) => {
+  res.send("API WORKING GREAT");
+});
+
+app.listen(port, () => console.log("Server runnig on port: ", port));
