80502a21a623a5673986df48df3000e75c3f657a
Nayan <dreamerboynayan@gmail.com>
2025-11-26 12:03:00 +0600

    Admin and Doctors Dashboard work start

---
+++ admin/src/App.jsx
@@ -0,0 +1,23 @@
+import React, { useContext } from 'react'
+import Login from './pages/Login'
+import { ToastContainer, toast } from 'react-toastify';
+import { AdminContext } from './context/AdminContext';
+import NavBar from './components/NavBar';
+
+const App = () => {
+  const {aToken} = useContext(AdminContext)
+  return aToken ? (
+    <div className='bg-[#f8f9fd]'>
+      
+      <ToastContainer/>
+      <NavBar/>
+    </div>
+  ) : (
+    <>
+    <Login/>
+      <ToastContainer/>
+    </>
+  )
+}
+
+export default App
