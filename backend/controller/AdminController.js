2b7ebf291c50d2c17b2cf8640691479377160054
Nayan <dreamerboynayan@gmail.com>
2025-12-05 11:23:27 +0600

    Admin Dashboard Admin  Appointment page work

--- backend/controller/AdminController.js
+++ backend/controller/AdminController.js
@@ -3,6 +3,7 @@ import bcrypt from "bcrypt";
 import { v2 as cloudinary } from "cloudinary";
 import doctorsModel from "../models/DoctorsModel.js";
 import jwt from 'jsonwebtoken'
+import appointmentModel from "../models/appointmentModel.js";
 
 // Api for adding doctors
 
@@ -120,4 +121,46 @@ const allDoctors = async (req,res) =>{
   }
 
 }
-export { addDoctor, loginAdmin , allDoctors};
+
+// API to get all Appointments list
+
+const appointmentsAdmin = async (req,res)=>{
+  try {
+    
+    const appointments = await appointmentModel.find({})
+    res.json({success:true,appointments})
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+}
+
+// API for appointment Cancellation 
+const appointmentCancel = async (req,res) =>{
+    try {
+        const { appointmentId} = req.body
+        
+        const appointmentData = await appointmentModel.findById(appointmentId)
+
+
+
+        await appointmentModel.findByIdAndUpdate(appointmentId,{cancelled:true})
+
+        // releasing doctor slot
+        const {docId, slotDate, slotTime} = appointmentData
+        const doctorData = await doctorsModel.findById(docId)
+
+        let slots_booked = doctorData.slots_booked
+
+        slots_booked[slotDate] = slots_booked[slotDate].filter(e=> e !== slotTime)
+
+        await doctorsModel.findByIdAndUpdate(docId,{slots_booked})
+
+        res.json({success:true,message:"Appointment Cancelled"})
+    } catch (error) {
+     console.log(error);
+    res.json({ success: false, message: error.message });
+    }
+
+}
+export { addDoctor, loginAdmin , allDoctors, appointmentsAdmin, appointmentCancel};
--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addDoctor, allDoctors, loginAdmin } from "../controller/AdminController.js";
+import { addDoctor, allDoctors, appointmentCancel, appointmentsAdmin, loginAdmin } from "../controller/AdminController.js";
 import upload from "../middleware/multer.js";
 import authAdmin from "../middleware/authAdmin.js";
 import { changeAvailablity } from "../controller/doctorController.js";
@@ -10,5 +10,7 @@ adminRouter.post("/add-doctor",authAdmin, upload.single("image"),addDoctor);
 adminRouter.post("/login", loginAdmin)
 adminRouter.post("/all-doctors", authAdmin,allDoctors)
 adminRouter.post("/change-availablity", authAdmin, changeAvailablity)
+adminRouter.get("/appointments", authAdmin, appointmentsAdmin)
+adminRouter.post("/cancel-appointment", authAdmin,appointmentCancel)
 
 export default adminRouter;
